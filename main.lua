0f4757a990eb55100b6a9eb18474309c5defd567
SpaceKiwi <SpaceKiwi@Miguels-iMac.local>
2025-11-02 11:38:14 +1300

    started working on the code to draw the walls around the player

--- main.lua
+++ main.lua
@@ -1,4 +1,14 @@
+function love.load()
+    mapData = {{1,1,1,1,1,1,1},{1,1,1,0,0,0,1},{1,1,1,0,1,0,1},{1,0,0,0,0,0,1},{1,0,1,0,1,1,1},{1,0,0,0,1,1,1},{1,1,1,1,1,1,1}}
+    wallTextures = {love.graphics.newImage("wallTextures/MissingTexture.png")}
+end
+
+
+
+
+
+
+
 function love.draw()
-    love.graphics.setColor(0,1,0)
-    love.graphics.rectangle("fill",0,0,200,100)
+    
 end
