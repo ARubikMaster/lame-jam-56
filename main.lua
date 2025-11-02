d098a0b58416dd04eee21da169d8e342f9f9aec1
SpaceKiwi <SpaceKiwi@Miguels-iMac.local>
2025-11-02 13:21:22 +1300

    bandaid patch for a crash

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ end
 
 function love.update()
     if love.keyboard.isDown("right") then -- not player movement just temporary for testing
-    playerX = playerX + 1 
+    playerX = playerX + 1
     end
 end
 
@@ -20,7 +20,7 @@ function love.draw()
     local TileOffsetY = (playerY % 16) * scaleY
     for y=1, math.min(love.graphics.getHeight() / (16 * scaleX) + 1,#mapData) do -- math.min and ,#mapData are just there to allow small map sizes and can be removed later
         for x=1, math.min(love.graphics.getWidth() / (16 * scaleY)  + 1,#mapData) do
-            if mapData[y + math.floor(playerY/16)][x + math.floor(playerX/16)] == 1 then -- actually draws the map
+            if mapData[math.min(math.max(y + math.floor(playerY/16),1),#mapData)][x + math.floor(playerX/16)] == 1 then -- actually draws the map
                 love.math.setRandomSeed((x+math.floor(playerX/16)) .. math.abs(y+math.floor(playerY/16)))
                 love.graphics.draw(wallTextures[love.math.random(1,#wallTextures)],((16 * x * scaleX) - TileOffsetX) - (16 * scaleX),(16 * y * scaleY) - TileOffsetY - (16 * scaleY),0,scaleX,scaleY)
             end
